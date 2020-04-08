c7a308d36d1d45c021f6c4bf24c9205c623858e2
BCG <bgould@users.noreply.github.com>
2020-04-08 00:59:00 -0400

    Switched to oofatfs

--- ff.h
+++ ff.h
@@ -1,8 +1,12 @@
+/* This file is part of ooFatFs, a customised version of FatFs
+ * See https://github.com/micropython/oofatfs for details
+ */
+
 /*----------------------------------------------------------------------------/
-/  FatFs - Generic FAT Filesystem module  R0.13                               /
+/  FatFs - Generic FAT Filesystem module  R0.13c                              /
 /-----------------------------------------------------------------------------/
 /
-/ Copyright (C) 2017, ChaN, all right reserved.
+/ Copyright (C) 2018, ChaN, all right reserved.
 /
 / FatFs module is an open source software. Redistribution and use of FatFs in
 / source and binary forms, with or without modification, are permitted provided
@@ -20,49 +24,80 @@
 
 
 #ifndef FF_DEFINED
-#define FF_DEFINED  87030   /* Revision ID */
+#define FF_DEFINED  86604   /* Revision ID */
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include "integer.h"    /* Basic integer types */
-#include "ffconf.h"     /* FatFs configuration options */
+#include "ffconf.h" //FFCONF_H       /* FatFs configuration options */
+#include "integer.h"
 
 #if FF_DEFINED != FFCONF_DEF
 #error Wrong configuration file (ffconf.h).
 #endif
 
 
+/* Integer types used for FatFs API */
+//
+//#if defined(_WIN32) /* Main development platform */
+//#define FF_INTDEF 2
+//#include <windows.h>
+//typedef unsigned __int64 QWORD;
+//#elif (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) || defined(__cplusplus)    /* C99 or later */
+//#define FF_INTDEF 2
+//#include <stdint.h>
+//typedef unsigned int    UINT;   /* int must be 16-bit or 32-bit */
+//typedef unsigned char   BYTE;   /* char must be 8-bit */
+//typedef uint16_t        WORD;   /* 16-bit unsigned integer */
+//typedef uint16_t        WCHAR;  /* 16-bit unsigned integer */
+//typedef uint32_t        DWORD;  /* 32-bit unsigned integer */
+//typedef uint64_t        QWORD;  /* 64-bit unsigned integer */
+//#else   /* Earlier than C99 */
+//#define FF_INTDEF 1
+//typedef unsigned int    UINT;   /* int must be 16-bit or 32-bit */
+//typedef unsigned char   BYTE;   /* char must be 8-bit */
+//typedef unsigned short  WORD;   /* 16-bit unsigned integer */
+//typedef unsigned short  WCHAR;  /* 16-bit unsigned integer */
+//typedef unsigned long   DWORD;  /* 32-bit unsigned integer */
+//#endif
+
 
 /* Definitions of volume management */
 
-#if FF_MULTI_PARTITION      /* Multiple partition configuration */
-typedef struct {
-    BYTE pd;    /* Physical drive number */
-    BYTE pt;    /* Partition: 0:Auto detect, 1-4:Forced partition) */
-} PARTITION;
-extern PARTITION VolToPart[];   /* Volume - Partition resolution table */
+#if FF_STR_VOLUME_ID
+#ifndef FF_VOLUME_STRS
+extern const char* VolumeStr[FF_VOLUMES];   /* User defied volume ID */
+#endif
 #endif
 
 
 
 /* Type of path name strings on FatFs API */
 
-#if FF_LFN_UNICODE && FF_USE_LFN    /* Unicode (UTF-16) string */
 #ifndef _INC_TCHAR
+#define _INC_TCHAR
+
+#if FF_USE_LFN && FF_LFN_UNICODE == 1   /* Unicode in UTF-16 encoding */
 typedef WCHAR TCHAR;
 #define _T(x) L ## x
 #define _TEXT(x) L ## x
-#define _INC_TCHAR
-#endif
-#else                       /* ANSI/OEM string */
-#ifndef _INC_TCHAR
+#elif FF_USE_LFN && FF_LFN_UNICODE == 2 /* Unicode in UTF-8 encoding */
+typedef char TCHAR;
+#define _T(x) u8 ## x
+#define _TEXT(x) u8 ## x
+#elif FF_USE_LFN && FF_LFN_UNICODE == 3 /* Unicode in UTF-32 encoding */
+typedef DWORD TCHAR;
+#define _T(x) U ## x
+#define _TEXT(x) U ## x
+#elif FF_USE_LFN && (FF_LFN_UNICODE < 0 || FF_LFN_UNICODE > 3)
+#error Wrong FF_LFN_UNICODE setting
+#else                                   /* ANSI/OEM code in SBCS/DBCS */
 typedef char TCHAR;
 #define _T(x) x
 #define _TEXT(x) x
-#define _INC_TCHAR
 #endif
+
 #endif
 
 
@@ -70,8 +105,8 @@ typedef char TCHAR;
 /* Type of file size variables */
 
 #if FF_FS_EXFAT
-#if !FF_USE_LFN
-#error LFN must be enabled when enable exFAT
+#if FF_INTDEF != 2
+#error exFAT feature wants C99 or later
 #endif
 typedef QWORD FSIZE_t;
 #else
@@ -83,8 +118,11 @@ typedef DWORD FSIZE_t;
 /* Filesystem object structure (FATFS) */
 
 typedef struct {
-    BYTE    fs_type;        /* Filesystem type (0:N/A) */
-    BYTE    pdrv;           /* Physical drive number */
+    void    *drv;           // block device underlying this filesystem
+#if FF_MULTI_PARTITION      /* Multiple partition configuration */
+    BYTE    part;           // Partition: 0:Auto detect, 1-4:Forced partition
+#endif
+    BYTE    fs_type;        /* Filesystem type (0:not mounted) */
     BYTE    n_fats;         /* Number of FATs (1 or 2) */
     BYTE    wflag;          /* win[] flag (b0:dirty) */
     BYTE    fsi_flag;       /* FSINFO flags (b7:disabled, b0:dirty) */
@@ -121,6 +159,9 @@ typedef struct {
     DWORD   fatbase;        /* FAT base sector */
     DWORD   dirbase;        /* Root directory base sector/cluster */
     DWORD   database;       /* Data base sector */
+#if FF_FS_EXFAT
+    DWORD   bitbase;        /* Allocation bitmap base sector */
+#endif
     DWORD   winsect;        /* Current sector appearing in the win[] */
     BYTE    win[FF_MAX_SS]; /* Disk access window for Directory, FAT (and file data at tiny cfg) */
 } FATFS;
@@ -133,7 +174,7 @@ typedef struct {
     FATFS*  fs;             /* Pointer to the hosting volume of this object */
     WORD    id;             /* Hosting volume mount ID */
     BYTE    attr;           /* Object attribute */
-    BYTE    stat;           /* Object chain status (b1-0: =0:not contiguous, =2:contiguous, =3:flagmented in this session, b2:sub-directory stretched) */
+    BYTE    stat;           /* Object chain status (b1-0: =0:not contiguous, =2:contiguous, =3:fragmented in this session, b2:sub-directory stretched) */
     DWORD   sclust;         /* Object data start cluster (0:no cluster or root directory) */
     FSIZE_t objsize;        /* Object size (valid when sclust != 0) */
 #if FF_FS_EXFAT
@@ -173,7 +214,7 @@ typedef struct {
 
 
 
-/* Directory object structure (DIR) */
+/* Directory object structure (FF_DIR) */
 
 typedef struct {
     FFOBJID obj;            /* Object identifier */
@@ -188,7 +229,7 @@ typedef struct {
 #if FF_USE_FIND
     const TCHAR* pat;       /* Pointer to the name matching pattern */
 #endif
-} DIR;
+} FF_DIR;
 
 
 
@@ -200,10 +241,10 @@ typedef struct {
     WORD    ftime;          /* Modified time */
     BYTE    fattrib;        /* File attribute */
 #if FF_USE_LFN
-    TCHAR   altname[13];            /* Altenative file name */
-    TCHAR   fname[FF_MAX_LFN + 1];  /* Primary file name */
+    TCHAR   altname[FF_SFN_BUF + 1];/* Altenative file name */
+    TCHAR   fname[FF_LFN_BUF + 1];  /* Primary file name */
 #else
-    TCHAR   fname[13];      /* File name */
+    TCHAR   fname[12 + 1];  /* File name */
 #endif
 } FILINFO;
 
@@ -239,40 +280,37 @@ typedef enum {
 /*--------------------------------------------------------------*/
 /* FatFs module application interface                           */
 
-FRESULT f_open (FIL* fp, const TCHAR* path, BYTE mode);             /* Open or create a file */
+FRESULT f_open (FATFS *fs, FIL* fp, const TCHAR* path, BYTE mode);  /* Open or create a file */
 FRESULT f_close (FIL* fp);                                          /* Close an open file object */
 FRESULT f_read (FIL* fp, void* buff, UINT btr, UINT* br);           /* Read data from the file */
 FRESULT f_write (FIL* fp, const void* buff, UINT btw, UINT* bw);    /* Write data to the file */
 FRESULT f_lseek (FIL* fp, FSIZE_t ofs);                             /* Move file pointer of the file object */
 FRESULT f_truncate (FIL* fp);                                       /* Truncate the file */
 FRESULT f_sync (FIL* fp);                                           /* Flush cached data of the writing file */
-FRESULT f_opendir (DIR* dp, const TCHAR* path);                     /* Open a directory */
-FRESULT f_closedir (DIR* dp);                                       /* Close an open directory */
-FRESULT f_readdir (DIR* dp, FILINFO* fno);                          /* Read a directory item */
-FRESULT f_findfirst (DIR* dp, FILINFO* fno, const TCHAR* path, const TCHAR* pattern);   /* Find first file */
-FRESULT f_findnext (DIR* dp, FILINFO* fno);                         /* Find next file */
-FRESULT f_mkdir (const TCHAR* path);                                /* Create a sub directory */
-FRESULT f_unlink (const TCHAR* path);                               /* Delete an existing file or directory */
-FRESULT f_rename (const TCHAR* path_old, const TCHAR* path_new);    /* Rename/Move a file or directory */
-FRESULT f_stat (const TCHAR* path, FILINFO* fno);                   /* Get file status */
-FRESULT f_chmod (const TCHAR* path, BYTE attr, BYTE mask);          /* Change attribute of a file/dir */
-FRESULT f_utime (const TCHAR* path, const FILINFO* fno);            /* Change timestamp of a file/dir */
-FRESULT f_chdir (const TCHAR* path);                                /* Change current directory */
-FRESULT f_chdrive (const TCHAR* path);                              /* Change current drive */
-FRESULT f_getcwd (TCHAR* buff, UINT len);                           /* Get current directory */
-FRESULT f_getfree (const TCHAR* path, DWORD* nclst, FATFS** fatfs); /* Get number of free clusters on the drive */
-FRESULT f_getlabel (const TCHAR* path, TCHAR* label, DWORD* vsn);   /* Get volume label */
-FRESULT f_setlabel (const TCHAR* label);                            /* Set volume label */
+FRESULT f_opendir (FATFS *fs, FF_DIR* dp, const TCHAR* path);       /* Open a directory */
+FRESULT f_closedir (FF_DIR* dp);                                    /* Close an open directory */
+FRESULT f_readdir (FF_DIR* dp, FILINFO* fno);                       /* Read a directory item */
+FRESULT f_findfirst (FF_DIR* dp, FILINFO* fno, const TCHAR* path, const TCHAR* pattern); /* Find first file */
+FRESULT f_findnext (FF_DIR* dp, FILINFO* fno);                      /* Find next file */
+FRESULT f_mkdir (FATFS *fs, const TCHAR* path);                     /* Create a sub directory */
+FRESULT f_unlink (FATFS *fs, const TCHAR* path);                    /* Delete an existing file or directory */
+FRESULT f_rename (FATFS *fs, const TCHAR* path_old, const TCHAR* path_new); /* Rename/Move a file or directory */
+FRESULT f_stat (FATFS *fs, const TCHAR* path, FILINFO* fno);        /* Get file status */
+FRESULT f_chmod (FATFS *fs, const TCHAR* path, BYTE attr, BYTE mask); /* Change attribute of a file/dir */
+FRESULT f_utime (FATFS *fs, const TCHAR* path, const FILINFO* fno); /* Change timestamp of a file/dir */
+FRESULT f_chdir (FATFS *fs, const TCHAR* path);                     /* Change current directory */
+FRESULT f_getcwd (FATFS *fs, TCHAR* buff, UINT len);                /* Get current directory */
+FRESULT f_getfree (FATFS *fs, DWORD* nclst);                        /* Get number of free clusters on the drive */
+FRESULT f_getlabel (FATFS *fs, TCHAR* label, DWORD* vsn);           /* Get volume label */
+FRESULT f_setlabel (FATFS *fs, const TCHAR* label);                 /* Set volume label */
 FRESULT f_forward (FIL* fp, UINT(*func)(const BYTE*,UINT), UINT btf, UINT* bf); /* Forward data to the stream */
-FRESULT f_expand (FIL* fp, FSIZE_t szf, BYTE opt);                  /* Allocate a contiguous block to the file */
-FRESULT f_mount (FATFS* fs, const TCHAR* path, BYTE opt);           /* Mount/Unmount a logical drive */
-FRESULT f_mkfs (const TCHAR* path, BYTE opt, DWORD au, void* work, UINT len);   /* Create a FAT volume */
-FRESULT f_fdisk (BYTE pdrv, const DWORD* szt, void* work);          /* Divide a physical drive into some partitions */
+FRESULT f_expand (FIL* fp, FSIZE_t fsz, BYTE opt);                  /* Allocate a contiguous block to the file */
+FRESULT f_mount (FATFS* fs);                                        /* Mount/Unmount a logical drive */
+FRESULT f_umount (FATFS* fs);                                       /* Unmount a logical drive */
+FRESULT f_mkfs (FATFS *fs, BYTE opt, DWORD au, void* work, UINT len); /* Create a FAT volume */
+FRESULT f_fdisk (void *pdrv, const DWORD* szt, void* work);         /* Divide a physical drive into some partitions */
 FRESULT f_setcp (WORD cp);                                          /* Set current code page */
-int f_putc (TCHAR c, FIL* fp);                                      /* Put a character to the file */
-int f_puts (const TCHAR* str, FIL* cp);                             /* Put a string to the file */
-int f_printf (FIL* fp, const TCHAR* str, ...);                      /* Put a formatted string to the file */
-TCHAR* f_gets (TCHAR* buff, int len, FIL* fp);                      /* Get a string from the file */
+FRESULT f_repair (FATFS* fs, void* work, UINT len);                 /* Free unreferenced clusters from the FAT */
 
 #define f_eof(fp) ((int)((fp)->fptr == (fp)->obj.objsize))
 #define f_error(fp) ((fp)->err)
@@ -299,10 +337,10 @@ DWORD get_fattime (void);
 #endif
 
 /* LFN support functions */
-#if FF_USE_LFN                      /* Code conversion (defined in unicode.c) */
+#if FF_USE_LFN >= 1                     /* Code conversion (defined in unicode.c) */
 WCHAR ff_oem2uni (WCHAR oem, WORD cp);  /* OEM code to Unicode conversion */
-WCHAR ff_uni2oem (WCHAR uni, WORD cp);  /* Unicode to OEM code conversion */
-WCHAR ff_wtoupper (WCHAR uni);          /* Unicode upper-case conversion */
+WCHAR ff_uni2oem (DWORD uni, WORD cp);  /* Unicode to OEM code conversion */
+DWORD ff_wtoupper (DWORD uni);          /* Unicode upper-case conversion */
 #endif
 #if FF_USE_LFN == 3                     /* Dynamic memory allocation */
 void* ff_memalloc (UINT msize);         /* Allocate memory block */
@@ -311,7 +349,7 @@ void ff_memfree (void* mblock);         /* Free memory block */
 
 /* Sync functions */
 #if FF_FS_REENTRANT
-int ff_cre_syncobj (BYTE vol, FF_SYNC_t* sobj); /* Create a sync object */
+int ff_cre_syncobj (FATFS *fatfs, FF_SYNC_t* sobj); /* Create a sync object */
 int ff_req_grant (FF_SYNC_t sobj);      /* Lock sync object */
 void ff_rel_grant (FF_SYNC_t sobj);     /* Unlock sync object */
 int ff_del_syncobj (FF_SYNC_t sobj);    /* Delete a sync object */
---
+++ go_fatfs.go
@@ -0,0 +1,528 @@
+package fatfs
+
+// #include <string.h>
+// #include <stdlib.h>
+// #include "./go_fatfs.h"
+import "C"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+	"unsafe"
+
+	gopointer "github.com/mattn/go-pointer"
+)
+
+const (
+	FileResultOK                          = C.FR_OK /* (0) Succeeded */
+	FileResultErr              FileResult = C.FR_DISK_ERR
+	FileResultIntErr           FileResult = C.FR_INT_ERR
+	FileResultNotReady         FileResult = C.FR_NOT_READY
+	FileResultNoFile           FileResult = C.FR_NO_FILE
+	FileResultNoPath           FileResult = C.FR_NO_PATH
+	FileResultInvalidName      FileResult = C.FR_INVALID_NAME
+	FileResultDenied           FileResult = C.FR_DENIED
+	FileResultExist            FileResult = C.FR_EXIST
+	FileResultInvalidObject    FileResult = C.FR_INVALID_OBJECT
+	FileResultWriteProtected   FileResult = C.FR_WRITE_PROTECTED
+	FileResultInvalidDrive     FileResult = C.FR_INVALID_DRIVE
+	FileResultNotEnabled       FileResult = C.FR_NOT_ENABLED
+	FileResultNoFilesystem     FileResult = C.FR_NO_FILESYSTEM
+	FileResultMkfsAborted      FileResult = C.FR_MKFS_ABORTED
+	FileResultTimeout          FileResult = C.FR_TIMEOUT
+	FileResultLocked           FileResult = C.FR_LOCKED
+	FileResultNotEnoughCore    FileResult = C.FR_NOT_ENOUGH_CORE
+	FileResultTooManyOpenFiles FileResult = C.FR_TOO_MANY_OPEN_FILES
+	FileResultInvalidParameter FileResult = C.FR_INVALID_PARAMETER
+	FileResultReadOnly         FileResult = 99
+
+	TypeFAT12 Type = C.FS_FAT12
+	TypeFAT16 Type = C.FS_FAT16
+	TypeFAT32 Type = C.FS_FAT32
+	TypeEXFAT Type = C.FS_EXFAT
+
+	AttrReadOnly  FileAttr = C.AM_RDO
+	AttrHidden    FileAttr = C.AM_HID
+	AttrSystem    FileAttr = C.AM_SYS
+	AttrDirectory FileAttr = C.AM_DIR
+	AttrArchive   FileAttr = C.AM_ARC
+
+	SectorSize = 512
+
+	FileAccessRead         OpenFlag = C.FA_READ
+	FileAccessWrite        OpenFlag = C.FA_WRITE
+	FileAccessOpenExisting OpenFlag = C.FA_OPEN_EXISTING
+	FileAccessCreateNew    OpenFlag = C.FA_CREATE_NEW
+	FileAccessCreateAlways OpenFlag = C.FA_CREATE_ALWAYS
+	FileAccessOpenAlways   OpenFlag = C.FA_OPEN_ALWAYS
+	FileAccessOpenAppend   OpenFlag = C.FA_OPEN_APPEND
+)
+
+type OpenFlag uint
+
+type Type uint
+
+func (t Type) String() string {
+	switch t {
+	case TypeFAT12:
+		return "FAT12"
+	case TypeFAT16:
+		return "FAT16"
+	case TypeFAT32:
+		return "FAT32"
+	case TypeEXFAT:
+		return "EXFAT"
+	default:
+		return "invalid/unknown"
+	}
+}
+
+type FileResult uint
+
+func (r FileResult) Error() string {
+	var msg string
+	switch r {
+	case FileResultErr:
+		msg = "(1) A hard error occurred in the low level disk I/O layer"
+	case FileResultIntErr:
+		msg = "(2) Assertion failed"
+	case FileResultNotReady:
+		msg = "(3) The physical drive cannot work"
+	case FileResultNoFile:
+		msg = "(4) Could not find the file"
+	case FileResultNoPath:
+		msg = "(5) Could not find the path"
+	case FileResultInvalidName:
+		msg = "(6) The path name format is invalid"
+	case FileResultDenied:
+		msg = "(7) Access denied due to prohibited access or directory full"
+	case FileResultExist:
+		msg = "(8) Access denied due to prohibited access"
+	case FileResultInvalidObject:
+		msg = "(9) The file/directory object is invalid"
+	case FileResultWriteProtected:
+		msg = "(10) The physical drive is write protected"
+	case FileResultInvalidDrive:
+		msg = "(11) The logical drive number is invalid"
+	case FileResultNotEnabled:
+		msg = "(12) The volume has no work area"
+	case FileResultNoFilesystem:
+		msg = "(13) There is no valid FAT volume"
+	case FileResultMkfsAborted:
+		msg = "(14) The f_mkfs() aborted due to any problem"
+	case FileResultTimeout:
+		msg = "(15) Could not get a grant to access the volume within defined period"
+	case FileResultLocked:
+		msg = "(16) The operation is rejected according to the file sharing policy"
+	case FileResultNotEnoughCore:
+		msg = "(17) LFN working buffer could not be allocated"
+	case FileResultTooManyOpenFiles:
+		msg = "(18) Number of open files > FF_FS_LOCK"
+	case FileResultInvalidParameter:
+		msg = "(19) Given parameter is invalid"
+	case FileResultReadOnly:
+		msg = "(99) Read-only filesystem"
+	default:
+		msg = "unknown file result error"
+	}
+	return "fatfs: " + msg
+}
+
+type FileAttr byte
+
+type Info struct {
+	size int64
+	name string
+	attr FileAttr
+}
+
+var _ os.FileInfo = (*Info)(nil)
+
+func (info *Info) Name() string {
+	return info.name
+}
+
+func (info *Info) Size() int64 {
+	return info.size
+}
+
+func (info *Info) IsDir() bool {
+	return (info.attr & AttrDirectory) > 0
+}
+
+func (info *Info) Sys() interface{} {
+	return nil
+}
+
+func (info *Info) Mode() os.FileMode {
+	v := os.FileMode(0777)
+	if info.IsDir() {
+		v |= os.ModeDir
+	}
+	return v
+}
+
+func (info *Info) ModTime() time.Time {
+	return time.Time{}
+}
+
+type FATFS struct {
+	fs  *C.FATFS
+	dev BlockDevice
+}
+
+type Config struct {
+	SectorSize int
+}
+
+func New(blockdev BlockDevice) *FATFS {
+	fs := &FATFS{
+		fs:  C.go_fatfs_new_fatfs(),
+		dev: blockdev,
+	}
+	fs.fs.drv = gopointer.Save(fs)
+	return fs
+}
+
+func (l *FATFS) Mount() error {
+	return errval(C.f_mount(l.fs))
+}
+
+func (l *FATFS) Format() error {
+	work := make([]byte, SectorSize)
+	return errval(C.f_mkfs(l.fs, C.FM_FAT, 0, unsafe.Pointer(&work[0]), C.UINT(len(work))))
+}
+
+func (l *FATFS) Free() (int64, error) {
+	var clust C.DWORD
+	res := C.f_getfree(l.fs, &clust)
+	if err := errval(res); err != nil {
+		return 0, err
+	}
+	return int64(clust * SectorSize), nil
+}
+
+func (l *FATFS) Unmount() error {
+	return nil
+}
+
+func (l *FATFS) Remove(path string) error {
+	cs := cstring(path)
+	defer C.free(unsafe.Pointer(cs))
+	return errval(C.f_unlink(l.fs, cs))
+}
+
+func (l *FATFS) Rename(oldPath string, newPath string) error {
+	cs1, cs2 := cstring(oldPath), cstring(newPath)
+	defer C.free(unsafe.Pointer(cs1))
+	defer C.free(unsafe.Pointer(cs2))
+	return errval(C.f_rename(l.fs, cs1, cs2))
+}
+
+func (l *FATFS) Stat(path string) (*Info, error) {
+	cs := cstring(path)
+	defer C.free(unsafe.Pointer(cs))
+	info := C.FILINFO{}
+	if err := errval(C.f_stat(l.fs, cs, &info)); err != nil {
+		return nil, err
+	}
+	return &Info{
+		size: int64(info.fsize),
+		name: gostring(&info.fname[0]),
+		attr: FileAttr(info.fattrib),
+	}, nil
+}
+
+func (l *FATFS) Mkdir(path string) error {
+	cs := cstring(path)
+	defer C.free(unsafe.Pointer(cs))
+	return errval(C.f_mkdir(l.fs, cs))
+}
+
+func (l *FATFS) Open(path string) (*File, error) {
+	return l.OpenFile(path, FileAccessRead)
+}
+
+func (l *FATFS) OpenFile(path string, flags OpenFlag) (*File, error) {
+
+	// TODO: better handling for paths
+	//for strings.HasPrefix(path, "/") {
+	//	path = strings.TrimLeft(path, "/")
+	//}
+	//println("opening:", path)
+
+	// create a C string with the file path
+	cs := cstring(path)
+	defer C.free(unsafe.Pointer(cs))
+
+	// stat the file path to see if it exists and if it is a file/dir
+	info := &C.FILINFO{}
+	if err := errval(C.f_stat(l.fs, cs, info)); err != nil && err != FileResultNoFile && err != FileResultInvalidName {
+		//println("warning:", err)
+		return nil, err
+	}
+
+	// use f_open or f_opendir to obtain a handle to the object
+	var file = &File{fs: l, name: path}
+	var errno C.FRESULT
+	if path == "/" || info.fattrib&C.AM_DIR > 0 {
+		// directory
+		println("attempting f_opendir:", gostring(cs))
+		file.typ = uint8(C.AM_DIR)
+		file.hndl = unsafe.Pointer(C.go_fatfs_new_ff_dir())
+		errno = C.f_opendir(l.fs, (*C.FF_DIR)(file.hndl), cs)
+	} else {
+		// file
+		println("attempting f_open:", gostring(cs))
+		file.typ = 0
+		file.hndl = unsafe.Pointer(C.go_fatfs_new_fil())
+		errno = C.f_open(l.fs, (*C.FIL)(file.hndl), cs, C.BYTE(flags))
+	}
+
+	// check to make sure f_open/f_opendir didn't produce an error
+	if err := errval(errno); err != nil {
+		if file.hndl != nil {
+			C.free(file.hndl)
+			file.hndl = nil
+		}
+		return nil, err
+	}
+
+	// file handle was initialized successfully
+	return file, nil
+}
+
+type File struct {
+	fs   *FATFS
+	typ  uint8
+	hndl unsafe.Pointer
+	name string
+}
+
+func (f *File) dirptr() *C.FF_DIR {
+	return (*C.FF_DIR)(f.hndl)
+}
+
+func (f *File) fileptr() *C.FIL {
+	return (*C.FIL)(f.hndl)
+}
+
+// Name returns the name of the file as presented to OpenFile
+func (f *File) Name() string {
+	return f.name
+}
+
+func (f *File) Close() error {
+	var errno C.FRESULT
+	if f.hndl != nil {
+		defer func() {
+			C.free(f.hndl)
+			f.hndl = nil
+		}()
+		if f.IsDir() {
+			errno = C.f_closedir(f.dirptr())
+		} else {
+			errno = C.f_close(f.fileptr())
+		}
+	}
+	return errval(errno)
+}
+
+func (f *File) Read(buf []byte) (n int, err error) {
+	if f.IsDir() {
+		return 0, FileResultInvalidObject
+	}
+	bufptr := unsafe.Pointer(&buf[0])
+	var br, btr C.UINT = 0, C.UINT(len(buf))
+	errno := C.f_read(f.fileptr(), bufptr, btr, &br)
+	if err := errval(errno); err != nil {
+		return int(br), err
+	}
+	if br == 0 && btr > 0 {
+		return 0, io.EOF
+	}
+	return int(br), nil
+}
+
+/*
+// Seek changes the position of the file
+func (f *File) Seek(offset int64, whence int) (ret int64, err error) {
+	errno := C.int(C.lfs_file_seek(f.lfs.lfs, &f.fptr, C.lfs_soff_t(offset), C.int(whence)))
+	if errno < 0 {
+		return -1, errval(errno)
+	}
+	return int64(errno), nil
+}
+
+// Tell returns the position of the file
+func (f *File) Tell() (ret int64, err error) {
+	errno := C.int(C.lfs_file_tell(f.lfs.lfs, &f.fptr))
+	if errno < 0 {
+		return -1, errval(errno)
+	}
+	return int64(errno), nil
+}
+
+// Rewind changes the position of the file to the beginning of the file
+func (f *File) Rewind() (err error) {
+	return errval(C.lfs_file_rewind(f.lfs.lfs, &f.fptr))
+}
+*/
+
+// Size returns the size of the file
+func (f *File) Size() (int64, error) {
+	if f.IsDir() {
+		ptr := f.dirptr()
+		return int64(ptr.obj.objsize), nil
+	} else {
+		ptr := f.fileptr()
+		return int64(ptr.obj.objsize), nil
+	}
+}
+
+// Synchronize a file on storage
+//
+// Any pending writes are written out to storage.
+// Returns a negative error code on failure.
+func (f *File) Sync() error {
+	return errval(C.f_sync(f.fileptr()))
+}
+
+/*
+// Truncates the size of the file to the specified size
+//
+// Returns a negative error code on failure.
+func (f *File) Truncate(size uint32) error {
+	return errval(C.lfs_file_truncate(f.lfs.lfs, &f.fptr, C.lfs_off_t(size)))
+}
+*/
+
+func (f *File) Write(buf []byte) (n int, err error) {
+	if f.IsDir() {
+		return 0, FileResultInvalidObject
+	}
+	bufptr := unsafe.Pointer(&buf[0])
+	var bw, btw C.UINT = 0, C.UINT(len(buf))
+	errno := C.f_write(f.fileptr(), bufptr, btw, &bw)
+	if err := errval(errno); err != nil {
+		return int(bw), err
+	}
+	if bw < btw {
+		return int(bw), errors.New("volume is full")
+	}
+	return int(bw), nil
+}
+
+func (f *File) IsDir() bool {
+	return f.typ == C.AM_DIR
+}
+
+func (f *File) Readdir(n int) (infos []os.FileInfo, err error) {
+	if !f.IsDir() {
+		return nil, FileResultInvalidObject
+	}
+	if n == 0 {
+		// passing nil pointer to f_readdir resets the read index
+		if err := errval(C.f_readdir(f.dirptr(), nil)); err != nil {
+			return nil, err
+		}
+	}
+	for {
+		info := C.FILINFO{}
+		if err := errval(C.f_readdir(f.dirptr(), &info)); err != nil {
+			return nil, err
+		}
+		if fname := gostring(&info.fname[0]); fname == "" {
+			return infos, nil
+		} else {
+			infos = append(infos, &Info{
+				size: int64(info.fsize),
+				name: fname,
+				attr: FileAttr(info.fattrib),
+			})
+		}
+	}
+}
+
+// would be nice to use C.CString instead, but TinyGo doesn't seem to support
+func cstring(s string) *C.char {
+	ptr := C.malloc(C.size_t(len(s) + 1))
+	buf := (*[1 << 28]byte)(ptr)[: len(s)+1 : len(s)+1]
+	copy(buf, s)
+	buf[len(s)] = 0
+	return (*C.char)(ptr)
+}
+
+// would be nice to use C.GoString instead, but TinyGo doesn't seem to support
+func gostring(s *C.char) string {
+	slen := int(C.strlen(s))
+	sbuf := make([]byte, slen)
+	copy(sbuf, (*[1 << 28]byte)(unsafe.Pointer(s))[:slen:slen])
+	return string(sbuf)
+}
+
+func errval(errno C.FRESULT) error {
+	if errno > FileResultOK {
+		return FileResult(errno)
+	}
+	return nil
+}
+
+// A BlockDevice is the raw device that is meant to store a filesystem.
+type BlockDevice interface {
+
+	// ReadAt reads the given number of bytes from the block device.
+	io.ReaderAt
+
+	// WriteAt writes the given number of bytes to the block device.
+	io.WriterAt
+
+	// Size returns the number of bytes in this block device.
+	Size() int64
+
+	// SectorSize returns the size of a single sector on this device.
+	SectorSize() int64
+
+	// EraseBlockSize returns the smallest erasable area on this particular chip
+	// in bytes. This is used for the block size in EraseBlocks.
+	// It must be a power of two, and may be as small as 1. A typical size is 4096.
+	EraseBlockSize() int64
+
+	// EraseBlocks erases the given number of blocks. An implementation may
+	// transparently coalesce ranges of blocks into larger bundles if the chip
+	// supports this. The start and len parameters are in block numbers, use
+	// EraseBlockSize to map addresses to blocks.
+	EraseBlocks(start, len int64) error
+
+	// Sync triggers the devices to commit any pending or cached operations
+	Sync() error
+}
+
+func xxdfprint(w io.Writer, offset uint32, b []byte) {
+	var l int
+	var buf16 = make([]byte, 16)
+	var padding = ""
+	for i, c := 0, len(b); i < c; i += 16 {
+		l = i + 16
+		if l >= c {
+			padding = strings.Repeat(" ", (l-c)*3)
+			l = c
+		}
+		fmt.Fprintf(w, "%08x: % x    "+padding, offset+uint32(i), b[i:l])
+		for j, n := 0, l-i; j < 16; j++ {
+			if j >= n {
+				buf16[j] = ' '
+			} else if !strconv.IsPrint(rune(b[i+j])) {
+				buf16[j] = '.'
+			} else {
+				buf16[j] = b[i+j]
+			}
+		}
+		w.Write(buf16)
+		fmt.Fprintln(w)
+	}
+}
